1d94d5ac8fce1b5f5d29164b13aaf93ad9a5a868
cloudfreexiao <996442717qqcom@gmail.com>
2018-11-24 14:11:01 +0800

    修改b3 uuid生成函数用系统自带

--- XIL/Assets/Plugins/AI/Behavior3Sharp/B3Functions.cs
+++ XIL/Assets/Plugins/AI/Behavior3Sharp/B3Functions.cs
@@ -5,20 +5,9 @@ namespace XIL.AI.Behavior3Sharp
 {
     public class B3Functions
     {
-        private static string hex_digits = "0123456789abcdef"; // { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-        private static readonly int m = 36;
-
         public static string CreateUUID()
         {
-            char[] s = new char[m];
-            for (int i = 0; i < m; i++)
-            {
-                s[i] = hex_digits[Random.range(0, hex_digits.Length - 1)];
-            }
-
-            s[8] = s[13] = s[18] = s[23] = '-';
-
-            return s.ToString();
+            return Guid.NewGuid().ToString();
         }
 
         //public static BaseNode CreateBehavior3Instance<T>(string classname)
